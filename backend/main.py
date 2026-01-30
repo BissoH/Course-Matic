bc2bd87905e37cd42284230b97315e2286b0cb69
BissoH <bissohatoum@icloud.com>
2026-01-30 15:22:43 +0000

    Implemented database functionality and authentication for login

---
+++ backend/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+
+import models
+import authentication
+from data import engine, get_db ,Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class UserAuth(BaseModel):
+    email: str
+    password: str
+
+@app.post("/register")
+def register(user: UserAuth, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    hashed_password = authentication.get_password_hash(user.password)
+    new_user = models.User(email=user.email, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return {"message": "User created", "email": new_user.email}
+
+
+@app.post("/login")
+def login(user : UserAuth, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if not db_user or not authentication.verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return {"message": "Login successful", "email": db_user.email}
